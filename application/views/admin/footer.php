7978620946e4d64f324966e97508ec99e4497de3
saefulbahrisaja@gmail.com <saefulbahrisaja@gmail.com>
2023-05-24 10:14:08 +0700

    Update footer side bar dan topbar

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -19,20 +19,20 @@
     <i class="fas fa-angle-up"></i>
   </a>
 
-  <!-- Logout Modal-->
+ 
   <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Yakin Logout?</h5>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-body">Pilih Tombol "Logout" untuk keluar sesi.</div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
+          <a class="btn btn-primary" href="<?= base_url('autentifikasi/logout'); ?>">Logout</a>
         </div>
       </div>
     </div>
@@ -53,6 +53,19 @@
   <!-- Page level custom scripts -->
   <script src="<?= base_url('assets/') ?>js/demo/chart-area-demo.js"></script>
   <script src="<?= base_url('assets/') ?>js/demo/chart-pie-demo.js"></script>
+
+  <script>
+    $('.custom-file-input').on('change',function(){
+      let fileName=$(this).val().split('\\').pop();
+      $(this).next('.custom-file-label').addclass("selected").html(fileName);
+    });
+
+    $(document).ready(function(){
+      $("#table-datatable").dataTable();
+    });
+
+    $('.alert-message').alert().delay(3500).slideup('slow')
+  </script>
 
 </body>
 
